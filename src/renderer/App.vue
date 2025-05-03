d60b7e22e55469dfa624ae167830ac86c07bd9cc
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-05-03 19:09:22 +0200

    refactor(renderer): destructure setProperty from style

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -4,26 +4,24 @@ import { RouterView } from 'vue-router';
 
 onBeforeMount(async () => {
   const { store } = window.electron;
-  const { style } = document.documentElement;
+  const { setProperty } = document.documentElement.style;
 
   const [font_family, font_size] = await Promise.all([store.getItem('font_family'), store.getItem('font_size')]);
 
   //
-  style.setProperty('--font-mono', font_family.monospace);
-  style.setProperty('--font-sans', font_family.sans_serif);
+  setProperty('--font-mono', font_family.monospace);
+  setProperty('--font-sans', font_family.sans_serif);
   store.onDidChange('font_family', ({ monospace, sans_serif }) => {
-    style.setProperty('--font-mono', monospace);
-    style.setProperty('--font-sans', sans_serif);
+    setProperty('--font-mono', monospace);
+    setProperty('--font-sans', sans_serif);
   });
 
   //
-  style.setProperty('font-size', `${font_size}px`);
-  store.onDidChange('font_size', font_size => style.setProperty('font-size', `${font_size}px`));
+  setProperty('font-size', `${font_size}px`);
+  store.onDidChange('font_size', font_size => setProperty('font-size', `${font_size}px`));
 });
 </script>
 
 <template>
   <RouterView />
 </template>
-
-<style scoped></style>
